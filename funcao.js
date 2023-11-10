926f93e86add0afb1ea3b5e840b602381fd20804
duardak <125658624+duardak@users.noreply.github.com>
2023-11-10 10:46:06 -0300

    Add files via upload

---
+++ funcao.js
@@ -0,0 +1,29 @@
+function LeiaSeuNome(autor){
+    if (autor == "Eduarda"){
+        leia("Viva a vida como se não houvesse um amanhã")
+    }
+
+}
+function LeiaSeuNome(autor){
+    if (autor == "Giuliana"){
+        leia("Mande embora o que não te faz bem")
+    }
+
+}
+function LeiaSeuNome(autor){
+    if (autor == "Samuel"){
+        leia("Chega de ficar de nhenhenhem")
+    }
+}
+function LeiaSeuNome(autor){
+    if (autor == "Eduardo"){
+        leia("Vou viver a vida nunca mais me estressar")
+    }
+}
+
+const autor["Eduarda", "Giuliana", "Samuel", "Eduardo"];
+let contador = 0;
+while(contador < autor.lenght){
+    leiaSeuNome(autor[contador]);
+    contador = contador = 1;
+}
